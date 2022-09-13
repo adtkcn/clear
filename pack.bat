b6839b9aa6e5ad8d88ca84baa067b93e35c4e008
heng <11675084@qq.com>
2022-09-13 13:55:51 +0800

    加入多平台打包命令

--- pack.bat
+++ pack.bat
@@ -1,18 +1,20 @@
-xcopy  .\static /S /Y .\bin\static
+xcopy  .\static /S /Y .\bin\clear-linux\static\
+xcopy  .\static /S /Y .\bin\clear-windows\static\
+xcopy  .\static /S /Y .\bin\clear-darwin\static\
+
 @REM # 1 目标平台的体系架构（386、amd64、arm） 
 set GOARCH=amd64
 @REM #2 目标平台的操作系统（darwin、freebsd、linux、windows）
 set GOOS=linux
-@REM #3 编译 使用-o指定你要生成的文件名称，勿需指定可以去掉（参考：go build main.go）
-go build -ldflags "-s -w"  -o ./bin/clear-linux
-.\upx.exe ./bin/clear-linux
+go build -ldflags "-s -w"  -o ./bin/clear-linux/clear
+.\upx.exe ./bin/clear-linux/clear
 
 @REM 打包window
 set GOOS=windows
-go build -ldflags "-s -w" -o ./bin/clear-windows.exe
-.\upx.exe ./bin/clear-windows.exe
+go build -ldflags "-s -w" -o ./bin/clear-windows/clear.exe
+.\upx.exe ./bin/clear-windows/clear.exe
 
 @REM 打包苹果darwin
 set GOOS=darwin
-go build -ldflags "-s -w" -o ./bin/clear-darwin
-.\upx.exe ./bin/clear-darwin
+go build -ldflags "-s -w" -o ./bin/clear-darwin/clear
+.\upx.exe ./bin/clear-darwin/clear
